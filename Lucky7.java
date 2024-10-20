6a395c5903659b08670e5914cc83b222e0a1f95f
epaco17 <amk1004065@student.hamk.fi>
2024-10-21 00:42:42 +0200

    User Input Implementation
    Implemented user input for spending and added replay functionality.

--- Lucky7.java
+++ Lucky7.java
@@ -1,9 +1,13 @@
 import java.util.Random;
+import java.util.Scanner;
 
 public class Lucky7 {
     public static void main(String[] args) {
         Random rand = new Random();
-        int money = 5;  // Player starts with 5€
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("How much do you want to spend? ");
+        int money = scanner.nextInt();  // Asking player how much to spend
 
         while (money > 0) {
             int num1 = rand.nextInt(10) + 1;
@@ -11,7 +15,7 @@ public class Lucky7 {
             int num3 = rand.nextInt(10) + 1;
 
             System.out.println("Your numbers are: " + num1 + ", " + num2 + ", " + num3);
-            money -= 1;  // Deduct 1€ per game
+            money -= 1;  // Deducting 1€ per game
 
             int count7 = 0;
             if (num1 == 7) count7++;
@@ -19,7 +23,7 @@ public class Lucky7 {
             if (num3 == 7) count7++;
 
             if (count7 == 1) {
-                System.out.println("You won 3 Euros!");
+                System.out.println("You won 3 Euros !");
                 money += 3;
             } else if (count7 == 2) {
                 System.out.println("You won 5 Euros!");
@@ -31,9 +35,17 @@ public class Lucky7 {
                 System.out.println("You lost this round.");
             }
 
-            System.out.println("Money left: " + money + " Euro\n");
+            System.out.println("Money left: " + money + " Euros");
+
+            if (money > 0) {
+                System.out.print("Do you want to play again? (yes/no): ");
+                String response = scanner.next();
+                if (!response.equalsIgnoreCase("yes")) {
+                    break;
+                }
+            }
         }
 
-        System.out.println("Game over! You have no money left.");
+        System.out.println("Game over! You finished with " + money + " Euros.");
     }
 }
